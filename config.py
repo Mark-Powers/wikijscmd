2fe62e6f232ea88b552d9bec295252e330e899d4
Mark Powers <mark@marks.kitchen>
2020-12-26 20:37:19 -0600

    Remove hardcoded path from config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
+import os
 from configparser import ConfigParser
 
-config_path = "/home/mark/dev/wikijscmd/config.ini"
-
+config_path = os.path.join(os.path.dirname(__file__), "config.ini")
 config = ConfigParser()
 config.read(config_path)
